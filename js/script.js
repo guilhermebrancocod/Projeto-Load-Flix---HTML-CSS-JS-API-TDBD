4721feed9feeefc34fe73544cd1c8884b5df670a
Guilherme <guilhermepbranco@hotmail.com>
2023-02-16 08:47:46 -0300

    outro commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch('https://api.themoviedb.org/3/movie/550?api_key=d34ce0ffbd6228a89395b248889b8991', {
+fetch('https://api.themoviedb.org/3/search/movie?api_key=d34ce0ffbd6228a89395b248889b8991', {
   method: 'GET',
   headers: {
     'Authorization': 'd34ce0ffbd6228a89395b248889b8991',
@@ -6,29 +6,76 @@ fetch('https://api.themoviedb.org/3/movie/550?api_key=d34ce0ffbd6228a89395b24888
   }
 })
 
-const MinhaApi_key = 'https://api.themoviedb.org/3/movie/550?api_key=d34ce0ffbd6228a89395b248889b8991'
+const MinhaApi_key = 'https://api.themoviedb.org/3/search/movie?api_key=d34ce0ffbd6228a89395b248889b8991'
 
-/*fetch(MinhaApi_key) Teste: Até aqui deu certo
-.then(response => response.json())
-.then(data => {
-  // manipula os dados retornados pela API
-  console.log(data);
-})
-.catch(error => {
-  console.error(error);
-});*/
+//Adicionando
+function adicionarItem(req, res) {
+  const novoItem = req.body;
+  MinhaApi_key.addItem(novoItem);
+  res.send('Item adicionado com sucesso');
+}
 
-const btn01 = document.getElementById('btnpesquisa');
-btn01.onclick = () => {
-  fetch(MinhaApi_key)
+//Removendo
+function removerItem(req, res) {
+  const id = req.params.id;
+  MinhaApi_key.removerItem(id);
+  res.send('Item removido com sucesso');
+}
+
+//Editando
+function editarItem(req, res) {
+  const id = req.params.id;
+  const dadosAtualizados = req.body;
+  MinhaApi_key.editarItem(id, dadosAtualizados);
+  res.send('Item atualizado com sucesso');
+}
+
+
+const frmPesquisa = document.querySelector("form")
+
+frmPesquisa.onsubmit = (ev) => {
+  ev.preventDefault();
+
+  const pesquisa = ev.target.pesquisa.value;
+
+  if(pesquisa == ""){
+    alert("Preencher o campo")
+    return;
+  }
+
+
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=d34ce0ffbd6228a89395b248889b8991&query=${pesquisa}`)
     .then(response => response.json())
-    .then(data => {
-      const userInfo = document.getElementById('card');
-      userInfo.innerHTML = `
-      <p>${data.id}</p>
-      <p>${data.original_title}</p>
-      <p>${data.release_date}</p>
-    `;
+    .then(json => carregaLista(json));
+}
+
+const carregaLista = (json) => {
+  const lista = document.querySelector("div.item");
+  lista.innerHTML = "";
+
+  if (json.success === null){
+    alert('Nenhum filme encontrado');
+    return
+  }
+
+  json.results.forEach(element => {
+
+    let item = document.createElement("div");
+    item.classList.add("card");
+
+    item.innerHTML = `<img src="${element.backdrop_path}" /><p class="title">${element.original_title}</p><p class="ano">Ano:${element.release_date}</p><button onclick="infoItem(${element.id})" class="infoItem">Sinopse</button><a href="#" class="trailer">trailer</a>`;
+
+    lista.appendChild(item);
+  });
+}
+const infoItem = (id) => {
+  fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=d34ce0ffbd6228a89395b248889b8991`)
+    .then(response => response.json())
+    .then(json => {
+      alert(`SINOPSE DO FILME SELECIONADO: ${json.overview}` );
     })
-    .catch(error => console.error(error));
 };
+
+
+
+
